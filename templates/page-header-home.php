68eeed0820a8f3ec053763bf1373b8cee9395e01
Ian Noyes <ian@12southmusic.com>
2014-03-26 17:19:44 -0500

    layerslider ph

--- templates/page-header-home.php
+++ templates/page-header-home.php
@@ -1,32 +1,6 @@
 <div class="page-header">
 <!--LayerSlider begin-->
-<div id="layerslider" style="width: 1000px; height: 500px;">
- 
-    <!--LayerSlider layer-->
-    <div class="ls-layer">
- 
-        <!--LayerSlider background-->
-        <img class="ls-bg" src=".." alt="layer1-background">
- 
-        <!--LayerSlider sublayers-->
-        <img class="ls-s-1" src=".." alt="layer1-sublayer1">
-        <img class="ls-s-1" src=".." alt="layer1-sublayer2">
-        <img class="ls-s-1" src=".." alt="layer1-sublayer3">
-    </div>
- 
-    <!--LayerSlider layer-->
-    <div class="ls-layer">
- 
-        <!--LayerSlider background-->
-        <img class="ls-bg" src=".." alt="layer2-background">
- 
-        <!--LayerSlider sublayers-->
-        <img class="ls-s1" src=".." alt="layer2-sublayer1">
-        <img class="ls-s2" src=".." alt="layer2-sublayer2">
-        <img class="ls-s3" src=".." alt="layer2-sublayer3">
-    </div>
- 
-</div>
+<?php layerslider(1, 'homepage'); ?>
 <!--LayerSlider end-->
   <h1>
     <?php echo roots_title(); ?>
