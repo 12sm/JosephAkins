67122711d6f7460a4773c3ba16eb7ebd1dd5882a
Ian Noyes <ian@12southmusic.com>
2014-03-27 16:11:40 -0500

    view now music-home

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-sm-12">
 		<div class="row">
-	<?php echo do_shortcode('[wpv-view name="music with Products"]') ?>
+	<?php echo do_shortcode('[wpv-view name="music-home"]') ?>
 		<?php echo do_shortcode('[wpv-view name="banners"]') ?>
 		</div>
 		<div class="row">
