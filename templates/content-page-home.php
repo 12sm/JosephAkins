ce074d830e83300717932a0d957e9290f7112686
Ian Noyes <ian@12southmusic.com>
2014-03-27 15:03:59 -0500

    rows in the home page

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,6 +1,8 @@
 <div class="row">
 	<div class="col-sm-12">
+		<div class="row">
 	<?php echo do_shortcode('[wpv-view name="music with Products"]') ?>
 		<?php echo do_shortcode('[wpv-view name="banners"]') ?>
+		</div>
 	</div>
 </div>
