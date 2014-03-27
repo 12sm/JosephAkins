c7568a556a89e7f8d7ead14d6af952d53245d621
Ian Noyes <ian@12southmusic.com>
2014-03-27 15:34:58 -0500

    owl-carousel in homepage

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -4,5 +4,10 @@
 	<?php echo do_shortcode('[wpv-view name="music with Products"]') ?>
 		<?php echo do_shortcode('[wpv-view name="banners"]') ?>
 		</div>
+		<div class="row">
+			<div class="col-sm-12">
+			<?php echo do_shortcode('[wpve-view name="music-carousel"]') ?>
+			</div>
+		</div>
 	</div>
 </div>
