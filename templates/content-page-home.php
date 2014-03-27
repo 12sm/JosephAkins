1b4c362cb1ce97eb8ceebb34124f93189c88c105
Ian Noyes <ian@12southmusic.com>
2014-03-27 13:03:23 -0500

    music with products view

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-sm-8">
-		<h1> Music Player</h1>
+	<?php echo do_shortcode('[wpv-view name="music with Products"]') ?>
 	</div>
 	<?php echo do_shortcode('[wpv-view name="banners"]') ?>
 </div>
