5fe997b7fbdbd4c30d03a3fab55398a47312622e
Ian Noyes <ian@12southmusic.com>
2014-03-27 13:35:46 -0500

    col's and such

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div class="col-sm-8">
+	<div class="col-sm-12">
 	<?php echo do_shortcode('[wpv-view name="music with Products"]') ?>
 	</div>
 	<?php echo do_shortcode('[wpv-view name="banners"]') ?>
