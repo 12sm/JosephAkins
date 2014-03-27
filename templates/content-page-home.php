dace3c31a4f1663259c4485f72fcb11104e58649
Ian Noyes <ian@12southmusic.com>
2014-03-27 14:57:33 -0500

    home-page template change

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-sm-12">
 	<?php echo do_shortcode('[wpv-view name="music with Products"]') ?>
+		<?php echo do_shortcode('[wpv-view name="banners"]') ?>
 	</div>
-	<?php echo do_shortcode('[wpv-view name="banners"]') ?>
 </div>
