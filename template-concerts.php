4d41152e4bdadd9e38eaf531313e006a60ba0d2b
Ian Noyes <ian@12southmusic.com>
2014-03-27 19:00:12 -0500

    Bandsintown stuffs

---
+++ template-concerts.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Concerts Template
+*/
+?>
+
+<?php get_template_part('templates/page', 'header-home'); ?>
+<?php get_template_part('templates/content', 'page-concerts'); ?>
---
+++ templates/content-page-concerts.php
@@ -0,0 +1,2 @@
+<script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
+<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-artist='Joseph Akins' data-text-color='#CCCCCC'>Bandsintown<a>
