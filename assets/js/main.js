49bb69bf102c296cce613549cfe23119cb978da9
Ian Noyes <ian@12southmusic.com>
2014-03-31 13:35:24 -0500

    do i even need audio.js?

--- assets/js/main.js
+++ assets/js/main.js
@@ -304,46 +304,7 @@ soundManager.setup({
     basicMP3Player = new BasicMP3Player();
   }
 });
-    
-
-
-  // ** Audio Player  ** //
-    
-    $(function() { 
-        // Setup the player to autoplay the next track
-        var a = audiojs.createAll({
-          trackEnded: function() {
-            var next = $('ol li.playing').next();
-            if (!next.length) next = $('ol li').first();
-            next.addClass('playing').siblings().removeClass('playing');
-            audio.load($('a', next).attr('data-src'));
-            audio.play();
-          }
-        });
-        
-        // Load in the first track
-        var audio = a[0];
-            first = $('ol a').attr('data-src');
-        $('ol li').first().addClass('playing');
-        audio.load(first);
-
-        // Load in a track on click
-        $('ol li').click(function(e) {
-          e.preventDefault();
-          $(this).addClass('playing').siblings().removeClass('playing');
-          audio.load($('a', this).attr('data-src'));
-          audio.play();
-        });
-        // Keyboard shortcuts
-        $(document).keydown(function(e) {
-          var unicode = e.charCode ? e.charCode : e.keyCode;
-             // space bar
-          if (unicode == 32) {
-            audio.playPause();
-          }
-        })
-      });
-}
+       
 },
 };
 // The routing fires all common scripts, followed by the page specific scripts.
