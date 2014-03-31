a2678e5f29adf422e8da05a7cb0b80eabfdd90c2
Ian Noyes <ian@12southmusic.com>
2014-03-31 13:09:42 -0500

    added audiojs and other things

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,44 @@ var Roots = {
 
   store:{
 init: function() {
+
+  // ** Audio Player  ** //
+    
+    $(function() { 
+        // Setup the player to autoplay the next track
+        var a = audiojs.createAll({
+          trackEnded: function() {
+            var next = $('ol li.playing').next();
+            if (!next.length) next = $('ol li').first();
+            next.addClass('playing').siblings().removeClass('playing');
+            audio.load($('a', next).attr('data-src'));
+            audio.play();
+          }
+        });
+        
+        // Load in the first track
+        var audio = a[0];
+            first = $('ol a').attr('data-src');
+        $('ol li').first().addClass('playing');
+        audio.load(first);
+
+        // Load in a track on click
+        $('ol li').click(function(e) {
+          e.preventDefault();
+          $(this).addClass('playing').siblings().removeClass('playing');
+          audio.load($('a', this).attr('data-src'));
+          audio.play();
+        });
+        // Keyboard shortcuts
+        $(document).keydown(function(e) {
+          var unicode = e.charCode ? e.charCode : e.keyCode;
+             // space bar
+          if (unicode == 32) {
+            audio.playPause();
+          }
+        })
+      });
+
     function InlinePlayer() {
   var self = this;
   var pl = this;
@@ -305,10 +343,9 @@ soundManager.onready(function() {
     inlinePlayer = new InlinePlayer();
   }
 });
-      }
-    }
-    };
-  }
+}
+}
+  
 
 
 // The routing fires all common scripts, followed by the page specific scripts.
