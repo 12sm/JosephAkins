33174da08c9290df74e5d9ab79dafc426e9a2e19
Ian Noyes <ian@12southmusic.com>
2014-04-03 17:47:28 -0500

    php conditional header

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,10 +2,13 @@
   <article <?php post_class(); ?>>
   <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
   <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5202adac0a506edf"></script>
-    <header>
+    <?php if (is_single('video')) {
+    } else { 
+    echo "<header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
-    </header>
+    </header>";
+    ?>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
