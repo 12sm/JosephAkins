c52b9afa0e9650fe0ac63d25acde640736981d01
Ian Noyes <ian@12southmusic.com>
2014-04-04 17:23:12 -0500

    photogal-template

---
+++ template-photogal.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Photo Gallery Template
+*/
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page-photogal'); ?>
