f6fc85919b3d9c7379801f13359c0e4ac84f1c7e
Ian Noyes <ian@12southmusic.com>
2014-04-09 16:27:29 -0500

    try this

--- functions.php
+++ functions.php
@@ -1,12 +1,3 @@
-
-
-<?php
-/*allow redirection, even if my theme starts to send output to the browser*/
-add_action('init', 'do_output_buffer');
-function do_output_buffer() {
-        ob_start();
-}
-?>
 <?php
 
 /*
