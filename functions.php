fab5f26d28b8a3e38319906c1a9915b4938293b4
Ian Noyes <ian@12southmusic.com>
2014-04-09 16:39:31 -0500

    hokay

--- functions.php
+++ functions.php
@@ -1,3 +1,18 @@
+
+<?php
+
+/*Add trim shortcode*/
+add_shortcode('trim', 'trim_shortcode');
+function trim_shortcode($atts, $content = '') {
+  $content = wpv_do_shortcode($content);
+  $length = (int)$atts['length'];
+  if (strlen($content) > $length) {
+    $content = substr($content, 0, $length) . '&hellip;';
+  }
+  return $content;
+}
+?>
+
 <?php
 
 /*
@@ -35,19 +50,7 @@ jQuery(document).ready(function() {
 });
 </script>
 
-<?php
 
-/*Add trim shortcode*/
-add_shortcode('trim', 'trim_shortcode');
-function trim_shortcode($atts, $content = '') {
-  $content = wpv_do_shortcode($content);
-  $length = (int)$atts['length'];
-  if (strlen($content) > $length) {
-    $content = substr($content, 0, $length) . '&hellip;';
-  }
-  return $content;
-}
-?>
 
 <?php
 }
