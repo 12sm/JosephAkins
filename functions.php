9f0c188832cac2b3244ca354cc018f8f9e8e924a
Ian Noyes <ian@12southmusic.com>
2014-04-09 16:24:14 -0500

    whoops

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 
-//allow redirection, even if my theme starts to send output to the browser
+
 <?php
+/*allow redirection, even if my theme starts to send output to the browser*/
 add_action('init', 'do_output_buffer');
 function do_output_buffer() {
         ob_start();
