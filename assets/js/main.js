68cb43cf21928726f84ff2eccd08d70aeb1950fa
Ian Noyes <ian@12southmusic.com>
2014-04-14 14:53:53 -0500

    imgliquid is back

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ var Roots = {
     init: function() {
       // JavaScript to be fired on all pages
       $('.vid-div').fitVids();
-      //$(".blog-img-container").imgLiquid();
+      $(".blog-img-container").imgLiquid();
        $('.thumbnail').colorbox();
     }
   },
