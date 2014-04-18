96b9ad6954d1df14a6ddf7371324146c334986f3
Ian Noyes <ian@12southmusic.com>
2014-04-18 18:17:30 -0500

    store templates

---
+++ content-page-store.php
@@ -0,0 +1,7 @@
+<div class="row">
+	<div class="col-sm-12">
+		<h3>Discograpy</h3>
+		<?php echo do_shortcode('[wpv-view name="music-carousel"]') ?>
+	</div>
+</div>
+<?php echo do_shortcode('[wpv-view name="Store-New"]')
---
+++ template-store.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Store Template
+*/
+?>
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page-store'); ?>
