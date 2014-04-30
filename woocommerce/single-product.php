9e6cb206276cf1ded6b52cbc1ce4524e483c27f4
Ian Noyes <ian@12southmusic.com>
2014-04-30 14:46:36 -0500

    debug

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,2 +1,3 @@
-<?php echo do_shortcode('[wpv-view name="single-product" ids="<?php get_the_ID(); ?>"]') ?>
-<h1><?php get_the_ID(); ?></h1>
+<?php $postid = get_the_ID(); ?> 
+<?php echo do_shortcode('[wpv-view name="single-product" ids="<?php echo $postid ?>"]') ?>
+<h1><?php echo $postid ?></h1>
