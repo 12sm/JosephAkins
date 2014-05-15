cfe5efa079ade71ba49c3293e8730342a8995d72
Ian Noyes <ian@12southmusic.com>
2014-05-15 10:39:19 -0500

    this too

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -2,6 +2,6 @@
 <div class="row">
 	<div class="col-sm-12">
 	<?php $postid = get_the_ID(); ?> 
-	<?php echo do_shortcode('[wpv-view name="single-product" ids=" '.$postid.' "]') ?>
+	<?php echo do_shortcode('[wpv-view name="single-product" ids=" '.$postid.' "]'); ?>
 	</div>
 </div>
