892079f244f08d8696c656550718aba3c4872a2e
michael <michael@12southmusic.com>
2014-05-31 13:55:35 -0400

    Try new SoundManager setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,8 +25,25 @@ var Roots = {
       // JavaScript to be fired on all pages
       $('.vid-div').fitVids();
       $(".blog-img-container").imgLiquid
-
      
+  soundManager.setup({
+  // disable or enable debug output
+  debugMode: true,
+  // use HTML5 audio for MP3/MP4, if available
+  preferFlash: false,
+  useFlashBlock: true,
+  // path to directory containing SM2 SWF
+  url: '/assets/js',
+  // optional: enable MPEG-4/AAC support (requires flash 9)
+  flashVersion: 9
+  });
+
+
+soundManager.onready(function() {
+  // soundManager.createSound() etc. may now be called
+  inlinePlayer = new InlinePlayer();
+});    
+
 
           }
   },
