176e9f3dcb72125fe7ff83f67b3bcf0c1cd52efd
michael <michael@12southmusic.com>
2014-06-05 13:32:18 -0500

    Update store template

--- templates/content-page-store.php
+++ templates/content-page-store.php
@@ -1,7 +1,1 @@
 <?php echo do_shortcode('[wpv-view name="Store-page"]') ?>
-<div class="row store-slider">
-	<div class="col-sm-12">
-		<h2>Discography</h2>
-		<?php echo do_shortcode('[wpv-view name="music-carousel"]') ?>
-	</div>
-</div>
