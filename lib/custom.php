24455827268906986360d72ca197e34aaf730aab
michael <michael@12southmusic.com>
2014-06-05 13:53:05 -0500

    Enter new short code

--- lib/custom.php
+++ lib/custom.php
@@ -2,4 +2,15 @@
 
 define( 'UPLOADS', ''.'assets' );
 
+function my_excerpt($atts) {
+extract(shortcode_atts(array(
+'length' => 20,
+'text' => '',
+), $atts));
+$excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+$res = wp_trim_words( $text, $length, $excerpt_more );
+return $res;
+}
+add_shortcode('my_excerpt', 'my_excerpt');
+
 ?>
