0109af4bb9a9c848b506c6928227a330697a6e3b
Sarah <Sarah@12southmusic.com>
2014-06-06 14:50:18 -0500

    bands in town color style

--- templates/content-page-concerts.php
+++ templates/content-page-concerts.php
@@ -1,2 +1,2 @@
 <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
-<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-artist='Joseph Akins' data-text-color='#CCCCCC'>Bandsintown<a>
+<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-artist='Joseph Akins' data-text-color='#A57513'>Bandsintown<a>
