aae79b4336b7f7ed421858c4863d362700130eac
michael <michael@12southmusic.com>
2014-06-06 12:27:06 -0500

    Remove after content

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -29,14 +29,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 		<?php endwhile; // end of the loop. ?>
 
-	<?php
-		/**
-		 * woocommerce_after_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
 
 
 <?php get_footer( 'shop' ); ?>
