7a74963da2f9a83ce5153612fe0512045d88cc5f
michael <michael@12southmusic.com>
2014-06-09 19:15:21 -0500

    Try new text function

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,11 +98,7 @@ soundManager.onready(function() {
           audio.load($('a', this).attr('data-src'));
           audio.play();
         });
-        // Keyboard shortcuts
-        //$(document).keydown(function(e) {
-          //var unicode = e.charCode ? e.charCode : e.keyCode;
-             // right arrow
-          //if (unicode == 39) {
+        
             $('.next').click(function() {
             	var next = $('li.playing').next();
             	next.click()
@@ -111,19 +107,12 @@ soundManager.onready(function() {
             	var prev = $('li.playing').prev();
             	prev.click()
             });
-            //if (!next.length) next = $('ol li').first();
-          
-            // back arrow
-          //} else if (unicode == 37) {
-          //  var prev = $('li.playing').prev();
-            //if (!prev.length) prev = $('ol li').last();
-            //prev.click();
-            // spacebar
-          //} else if (unicode == 32) {
-            //audio.playPause();
-          //}
-        //})
-    
+            
+            $('.song-list li').click(function(){
+            	var newtext = $('li.playing a').text();
+	            $('.music-wrapper p').text(newtext);
+            });
+             
     $('.add_to_cart_button').removeClass('button');
     }
   },
