2102d3bde13fbd7591fdafb2bcc7d82ad369c3c0
michael <michael@12southmusic.com>
2014-06-11 14:57:33 -0500

    Home featured excerpt fix on smaller

--- templates/content-page-blog.php
+++ templates/content-page-blog.php
@@ -1,3 +1,3 @@
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":false};</script>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5202adac0a506edf"></script>
 <?php echo do_shortcode('[wpv-view name="Blog"]'); ?>
