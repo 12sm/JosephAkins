e23bcbffa86560c4e6cd2d73b65ed56846cc7171
michael <michael@12southmusic.com>
2014-06-11 12:37:57 -0500

    Update Google Analytics and tab script

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,10 +42,23 @@ var Roots = {
 soundManager.onready(function() {
   // soundManager.createSound() etc. may now be called
   inlinePlayer = new InlinePlayer();
-});    
+});   
+
+$(document).ready(function() {
+    if(location.hash) {
+       $('a[href=' + location.hash + ']').tab('show');
+    }
+  $(document.body).on("click", "a[data-toggle]", function(event) {
+    location.hash = this.getAttribute("href");
+  });
+});
+  $(window).on('popstate', function() {
+    var anchor = location.hash || $("a[data-toggle=tab]").first().attr("href");
+    $('a[href=' + anchor + ']').tab('show');
+  }); 
 
 
-          }
+}
   },
 
   // Media page
