fda054aec1800b27bb4f89d6beef88679dbb8d1c
michael <michael@12southmusic.com>
2014-06-11 15:31:59 -0500

    Change bandsintown size

--- templates/content-page-concerts.php
+++ templates/content-page-concerts.php
@@ -1,2 +1,2 @@
 <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
-<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-artist='Joseph Akins' data-text-color='#000000' data-link-color='#A57513'>Bandsintown<a>
+<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-with="100%" data-force-narrow-layout="true" data-artist='Joseph Akins' data-text-color='#000000' data-link-color='#A57513'>Bandsintown<a>
