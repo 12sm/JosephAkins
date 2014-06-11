3872dbae75b21396a19f1853229c259ac10ecea4
michael <michael@12southmusic.com>
2014-06-11 15:35:13 -0500

    Wrong classes

--- templates/content-page-concerts.php
+++ templates/content-page-concerts.php
@@ -1,9 +1,9 @@
-<div class="hidden-xs hidden-sm">
+<div class="hidden-md hidden-lg">
 	<script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
 		<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-with="100%" data-force-narrow-layout="true" data-artist='Joseph Akins' data-text-color='#000000' data-link-color='#A57513'>Bandsintown</a>
 </div>
 
-<div class="hidden-md hidden-lg">
+<div class="hidden-xs hidden-sm">
 	<script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
 		<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-with="100%" data-artist='Joseph Akins' data-text-color='#000000' data-link-color='#A57513'>Bandsintown</a>
 </div>
